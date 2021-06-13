b75cd0a45c12f7cf26020c467bc5a0433fb1458f
Rick Izzo <rlizzo@users.noreply.github.com>
2021-06-13 06:56:57 -0400

    Update demo.jl

--- demo.jl
+++ demo.jl
@@ -1,10 +1,12 @@
-using LinearAlgebra, Statistics
+using LinearAlgebra, Statistics, Printf
 # commandline arguments.  In reality, use ArgParse equivalents
 #
 num = parse(Int64,ARGS[2])
 ϵ = randn(num)
-println("mean = $(mean(ϵ)), std = $(std(ϵ)) of $num values")
+m = mean(ϵ)
+s = std(ϵ)
+println("mean = $m, std = $s of $num values")
 
 open("myout.txt","w") do f 
-    write(f, sprintf "mean = $(mean(ϵ)), std = $(std(ϵ)) of $num values")
+    write(f, @sprintf("mean = %s, std = %s of %s\n", m, s, num))
 end
