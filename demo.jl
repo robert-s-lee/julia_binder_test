fa2d8028ba82404d28f8044861f7840671af0b5b
Robert S Lee <sangkyulee@gmail.com>
2022-01-28 15:37:47 -0500

    Update demo.jl

--- demo.jl
+++ demo.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra, Statistics
 # commandline arguments.  In reality, use ArgParse equivalents
+print(ARGS)
 num = parse(Int64,ARGS[2])
 ϵ = randn(num)
 println("mean = $(mean(ϵ)), std = $(std(ϵ)) of $num values")
